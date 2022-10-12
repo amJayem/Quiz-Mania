9680ac9858fecf890dab6f6b8050d489236d883a
amJayem <csejayem19@gmail.com>
2022-10-12 18:53:20 +0600

    set active link

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       element: <Main></Main>,
       children: [
         {
-          path: '/',
+          path: '/home',
           element: <Home></Home>,
           loader: (()=> fetch(url))
         },
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
 
 /**
  * sm:items-center sm:justify-between
  * bg-gradient-to-r from-blue-500 text-slate-700
  */
+
 const Header = () => {
     return (
         <nav className=' nav mb-10 p-5 '>
             <div>
-                <NavLink to='/' className='font-bold text-2xl text-black'>
+                <Link to='/home' className='font-bold text-2xl text-black'>
                     Quiz Mania
-                </NavLink>
+                </Link>
             </div>
-            <div className=' '>
-                <NavLink className='font-bold mr-2' to='/'>Home</NavLink>            
-                <NavLink className='font-bold mr-2' to='/quiz/1'>Quiz</NavLink>
-                <NavLink className='font-bold mr-2' to='/statistics'>Show Statistics</NavLink>
-                <NavLink className='font-bold mr-2' to='/blog'>Blog</NavLink>
+            <div className='nav-link'>
+                <NavLink className={({isActive})=> isActive ? 'active': 'undefined'
+                //'bg-blue-400 p-3 rounded-md text-white'
+            }  to='/home'>Home</NavLink>            
+                <NavLink  to='/quiz/1'>Quiz</NavLink>
+                <NavLink  to='/statistics'>Show Statistics</NavLink>
+                <NavLink  to='/blog'>Blog</NavLink>
             </div>
         </nav>
     );
